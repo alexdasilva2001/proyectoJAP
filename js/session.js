175202b660b9ef5ed5f8dccffc641e9b08322eb8
mateobertoletti <mateobertoletti30@gmail.com>
2025-08-30 17:14:04 -0300

    Usuario en barra

--- js/session.js
+++ js/session.js
@@ -1,17 +1,23 @@
-if (localStorage.getItem("logueado") === "true" && window.location.pathname.includes("login.html")) {
+if (
+  localStorage.getItem("logueado") === "true" &&
+  window.location.pathname.includes("login.html")
+) {
   window.location.href = "index.html";
 }
 
-const form = document.querySelector("form"); 
-form.addEventListener("submit", function(event) {
-  const usuario = document.getElementById('usuario').value.trim();
-  const contraseña = document.getElementById('contraseña').value.trim();
+const form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+  const usuario = document.getElementById("usuario").value.trim();
+  const contraseña = document.getElementById("contraseña").value.trim();
   if (usuario !== "" && contraseña !== "") {
     localStorage.setItem("logueado", "true");
     localStorage.setItem("usuario", usuario);
   }
 });
-const usuarioGuardado = localStorage.getItem("usuario");
-if (usuarioGuardado) {
-  document.getElementById('usuariobarra').innerText = usuarioGuardado;
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const usuarioGuardado = localStorage.getItem("usuario");
+  if (usuarioGuardado) {
+    document.getElementById("usuariobarra").innerHTML = usuarioGuardado;
+    document.getElementById("usuarioguardado").innerHTML = usuarioGuardado;
+  }
+});
